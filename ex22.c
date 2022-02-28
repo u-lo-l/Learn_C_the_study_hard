c207a93901167445ff2a70b7abaa57d6d56938b2
u-lo-l <jeffk0408@naver.com>
2022-02-28 13:17:08 +0900

    22~24

---
+++ ex22.c
@@ -0,0 +1,32 @@
+#include "dbg.h"
+#include "ex22.h"
+#include <stdio.h>
+
+int THE_SIZE = 100;
+
+static int THE_AGE = 37;
+
+int get_age()
+{
+	return THE_AGE;
+}
+
+void set_age(int age)
+{
+	THE_AGE = age;
+}
+
+double update_ratio(double new_ratio)
+{
+	static double ratio = 1.0;
+
+	double old_ratio = ratio;
+	ratio = new_ratio;
+
+	return old_ratio;
+}
+
+void print_size()
+{
+	log_info("I think size is : %d", THE_SIZE);
+}
---
+++ ex22.h
@@ -0,0 +1,13 @@
+#ifndef _ex22_h_
+# define _ex22_h_
+
+extern int THE_SIZE;
+
+int get_age();
+void set_age(int age);
+
+double update_ratio(double ratio);
+
+void print_size();
+
+#endif
